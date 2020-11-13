293a291a4c8b42556f4d74ba4da9c1b1c8810d5e
JakeSeib <jacob.a.seib@gmail.com>
2020-11-13 13:00:24 -0500

    Passlock styling

--- src/components/PassLock/PassLock.js
+++ src/components/PassLock/PassLock.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import './PassLock.scss'
 
 const PassLock = ({ setLocked, password }) => {
   const [passwordEntry, setPasswordEntry] = useState('')
@@ -27,22 +28,23 @@ const PassLock = ({ setLocked, password }) => {
 
   let passLockJSX
   if (passwordCorrect) {
-    passLockJSX = <p>Password Accepted. Welcome, Glorbo.</p>
+    passLockJSX = <p className='password-accept-msg'>Password Accepted. Welcome, Glorbo.</p>
   } else {
     passLockJSX = <Fragment>
       <Form className='password-form' onSubmit={handleFormSubmit}>
-        <Form.Group controlId="password">
+        <Form.Group controlId='password'>
           <Form.Label>Enter Password</Form.Label>
           <Form.Control
+            className='password-field'
             required
-            type="password"
-            name="password"
+            type='password'
+            name='password'
             value={passwordEntry}
             onChange={handleChange}
-            maxLength="10"
+            maxLength='10'
           />
         </Form.Group>
-        <Button variant="success" type="submit">Unlock</Button>
+        <Button className='button-password' variant='success' size='lg' type='submit'>Unlock</Button>
       </Form>
       <p>{passwordCorrect === false ? 'Incorrect Password' : ''}</p>
     </Fragment>
